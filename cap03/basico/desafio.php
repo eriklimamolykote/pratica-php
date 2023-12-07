239939eac0961f77a82f31ec7c53c201b679323e
Erik Augusto Lima Evangelista <eriklim@hotmail.com>
2023-12-07 10:19:49 -0300

    commit-pratica-php-07-12-2023

---
+++ cap03/basico/desafio.php
@@ -0,0 +1,22 @@
+<div class="titulo">Desafio</div>
+
+<ul>
+    <li> 1 + 1 = ?</li>
+    <li> 4 + 4 = ?</li>
+    <li> 8 + 8 = ?</li>
+</ul>
+
+<br>
+<div class="titulo">Resposta</div>
+    <ul>
+        <li>
+            <?= 1 + 1 ?>
+        </li>
+        <li>
+            <?php echo 4 + 4; ?>
+        </li>
+        <li>
+            <?= "1" ?><?php echo 3 + 3; ?>
+        </li>
+    </ul>
+</div>
